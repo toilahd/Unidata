53f586f7b039048ea61fc5a9a4259a4bbd41a5be
TRIETDINHDUY <trietdd.ss@gmail.com>
2025-04-09 22:28:54 +0700

    verify user

--- src/main/java/com/example/unidata/HelloController.java
+++ src/main/java/com/example/unidata/HelloController.java
@@ -1,14 +1,108 @@
 package com.example.unidata;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class HelloController {
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private TextField username;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private PasswordField password;
+
+    @FXML
+    private Button button_login;
+
+    //DATABASE TOOLS
+    private Connection connect;
+    private PreparedStatement prepare;
+    private ResultSet result;
+
+    //CREATE DATABASE
+    public void loginAdmin() {
+        String sql = "SELECT * FROM ACCOUNT WHERE username = ? and password = ?";
+        DatabaseConnection database;
+        connect = DatabaseConnection.connectDb();
+
+        try {
+            Alert alert;
+
+            prepare = connect.prepareStatement(sql);
+            prepare.setString(1, username.getText());
+            prepare.setString(2, password.getText());
+
+            result = prepare.executeQuery();
+
+            if(username.getText().isEmpty() || password.getText().isEmpty()) {
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Please fill all blank fields!");
+                alert.showAndWait();
+            } else {
+                if(result.next()) {
+                    String role = result.getString("VAITRO");
+
+                    //Proceed To Next Scene
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully Login!");
+                    alert.showAndWait();
+
+                    button_login.getScene().getWindow().hide();
+                    //LINK TO NEXT SCENE
+                    String fxmlFile;
+                    switch (role.toLowerCase()) {
+                        case "admin":
+                            fxmlFile = "admin.fxml";
+                            break;
+                        case "teacher":
+                            fxmlFile = "teacher.fxml";
+                            break;
+                        case "student":
+                            fxmlFile = "student.fxml";
+                            break;
+                        default:
+                            fxmlFile = "default.fxml"; // optional fallback
+                            break;
+                    }
+
+                    Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+
+                }else{
+                    //Send Error Message
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Wrong Username/Password");
+                    alert.showAndWait();
+                }
+            }
+
+        }catch (Exception e) {e.printStackTrace();}
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.example.unidata {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires java.desktop;
+    requires com.oracle.database.jdbc;
 
 
     opens com.example.unidata to javafx.fxml;
